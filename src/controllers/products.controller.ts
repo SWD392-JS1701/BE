6538719d953d3b86e4f9a15d9239ec4a65ca32c8
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-02-11 10:21:59 +0700

    add login

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,13 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Put,
-  Delete,
-} from '@nestjs/common';
-import { ProductsService, Product } from './products.service'; // Import Product
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common'
+import { ProductsService, Product } from '../services/products.service' // Import Product
 
 @Controller('products')
 export class ProductsController {
@@ -15,26 +7,26 @@ export class ProductsController {
 
   @Get()
   findAll(): Product[] {
-    return this.productsService.findAll();
+    return this.productsService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: number): Product | undefined {
-    return this.productsService.findOne(id);
+    return this.productsService.findOne(id)
   }
 
   @Post()
   create(@Body() product: Omit<Product, 'id'>): void {
-    this.productsService.create(product);
+    this.productsService.create(product)
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() product: Omit<Product, 'id'>): void {
-    this.productsService.update(id, product);
+    this.productsService.update(id, product)
   }
 
   @Delete(':id')
   remove(@Param('id') id: number): void {
-    this.productsService.remove(id);
+    this.productsService.remove(id)
   }
 }
