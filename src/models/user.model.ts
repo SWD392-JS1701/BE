479fedfbafff5a2d072178804e89ca83ad020e08
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-18 13:25:43 +0700

    create user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ type: Types.ObjectId, auto: true })
+  _id!: Types.ObjectId;
+
+  @Prop({ required: true, unique: true })
+  username!: string;
+
+  @Prop({ required: true })
+  password_hash!: string;
+
+  @Prop()
+  date_of_birth!: Date;
+
+  @Prop({ required: true, unique: true })
+  email!: string;
+
+  @Prop({ default: 'User' }) 
+  role!: string;
+
+  @Prop()
+  skin_type?: string;
+
+  @Prop()
+  membership_id!: number;
+
+  @Prop({ default: 0 })
+  point!: number;
+
+  @Prop()
+  first_name!: string;
+
+  @Prop()
+  last_name!: string;
+
+  @Prop()
+  phone_number?: string;
+
+  @Prop()
+  address?: string;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- src/modules/products.module.ts
+++ src/modules/products.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ProductsService } from '../services/products.service'
 import { ProductsController } from '../controllers/products.controller'
-import { Product, ProductSchema } from '../models/products.model'
+import { Product, ProductSchema } from '../models/product.model'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
