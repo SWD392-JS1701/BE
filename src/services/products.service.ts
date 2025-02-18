2c833dd0a4e820201aa7fb3db1b44bc2139bc656
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-02-19 02:27:44 +0700

    add product validation

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,30 +1,83 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, BadRequestException } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model, UpdateQuery } from 'mongoose'
 import { Product, ProductDocument } from '../models/products.model'
+
+interface ProductDTO {
+  name: string
+  product_rating?: number
+  description?: string
+  price: number
+  stock: number
+  product_type_id: number
+  image_url?: string
+  Supplier?: string
+  expired_date?: Date
+  volume?: number
+}
 
 @Injectable()
 export class ProductsService {
   constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) {}
 
   async findAll(): Promise<Product[]> {
-    return this.productModel.find().exec()
+    return await this.productModel.find().exec()
   }
 
   async findOne(id: string): Promise<Product | null> {
-    return this.productModel.findById(id).exec()
+    return await this.productModel.findById(id).exec()
   }
 
-  async create(createProductDto: any): Promise<Product> {
+  async findByName(name: string): Promise<Product[]> {
+    return await this.productModel.find({ name: { $regex: new RegExp(name, 'i') } }).exec()
+  }
+
+  async findByExactName(name: string): Promise<Product[]> {
+    return await this.productModel.find({ name: { $regex: new RegExp(`^${name}$`, 'i') } }).exec()
+  }
+
+  async create(createProductDto: ProductDTO): Promise<Product> {
+    await this.validateProduct(createProductDto)
     const createdProduct = new this.productModel(createProductDto)
     return createdProduct.save()
   }
 
-  async update(id: string, updateProductDto: UpdateQuery<Product>): Promise<Product | null> {
+  async update(id: string, updateProductDto: UpdateQuery<ProductDTO>): Promise<Product | null> {
+    await this.validateProduct(updateProductDto)
     return this.productModel.findByIdAndUpdate(id, updateProductDto, { new: true }).exec()
   }
 
   async remove(id: string): Promise<Product | null> {
     return this.productModel.findByIdAndDelete(id).exec()
+  }
+
+  private async validateProduct(product: Partial<ProductDTO>) {
+    if (!product.name || typeof product.name !== 'string') {
+      throw new BadRequestException('Invalid product name')
+    }
+    if ((await this.findByExactName(product.name))[0] != null) {
+      throw new BadRequestException('Product name existed' + product.name)
+    }
+    if (
+      product.product_rating !== undefined &&
+      (typeof product.product_rating !== 'number' || product.product_rating < 0 || product.product_rating > 5)
+    ) {
+      throw new BadRequestException('Invalid product rating')
+    }
+    if (typeof product.price !== 'number' || product.price < 0) {
+      throw new BadRequestException('Invalid price')
+    }
+    if (typeof product.stock !== 'number' || product.stock < 0) {
+      throw new BadRequestException('Invalid stock')
+    }
+    if (typeof product.product_type_id !== 'number') {
+      throw new BadRequestException('Invalid product type ID')
+    }
+    if (product.expired_date && isNaN(Date.parse(product.expired_date.toString()))) {
+      throw new BadRequestException('Invalid expired date')
+    }
+    if (product.volume !== undefined && (typeof product.volume !== 'number' || product.volume < 0)) {
+      throw new BadRequestException('Invalid volume')
+    }
   }
 }
