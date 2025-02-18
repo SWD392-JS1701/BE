c8fe2a40d82b7bac2d00b6b41293837bd54f529d
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-19 01:41:10 +0700

    create register api

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { CreateUserDto } from '../dtos/user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,5 +9,10 @@ export class AuthController {
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadGatewayException, BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { User, UserDocument } from '../models/user.model';
+import { CreateUserDto } from '../dtos/user.dto';
 
 @Injectable()
 export class AuthService {
@@ -19,7 +20,6 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    // Compare the provided password with the stored password hash
     const isPasswordValid = await bcrypt.compare(password, user.password_hash);
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
@@ -29,5 +29,34 @@ export class AuthService {
     const access_token = this.jwtService.sign(payload);
 
     return { access_token };
+  }
+
+  async register(createUserDto: CreateUserDto): Promise<{ message: string; access_token?: string }> {
+    const { username, email, password, ...otherFields } = createUserDto;
+
+    const existingUser = await this.userModel.findOne({
+      $or: [{ username }, { email }],
+    }).exec();
+
+    if (existingUser) {
+      throw new BadRequestException('User with the provided username or email already exists');
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt();
+    const password_hash = await bcrypt.hash(password, salt);
+
+    const createdUser = new this.userModel({
+      username,
+      email,
+      password_hash,
+      ...otherFields,
+    });
+
+    await createdUser.save();
+
+     const payload = { username: createdUser.username, id: createdUser._id };
+     const access_token = this.jwtService.sign(payload);
+     return { message: 'Registration successful', access_token };
   }
 }
