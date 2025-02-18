bb94bd8f573742f84287fe63ad40b25a5f860da9
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-18 10:00:57 +0700

    connect to db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app.module'
+import * as dotenv from 'dotenv';
+import { ChildProcess } from 'child_process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  dotenv.config();
   await app.listen(process.env.PORT ?? 4000)
 }
 bootstrap()
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common'
-import { AppController } from '../controllers/app.controller'
-import { AppService } from '../services/app.service'
-import { ProductsModule } from './products.module'
-import { AuthModule } from './auth.module'
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from '../controllers/app.controller';
+import { AppService } from '../services/app.service';
+import { ProductsModule } from './products.module';
+import { AuthModule } from './auth.module';
 
 @Module({
-  imports: [ProductsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    ProductsModule,
+    AuthModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
