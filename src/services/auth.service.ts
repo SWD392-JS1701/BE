06c4570775f25e3952ed938fbec4d9c06cc1579e
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-19 00:17:54 +0700

    login controller, service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,16 +1,33 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from '../models/user.model';
 
 @Injectable()
 export class AuthService {
-  private readonly validUsername = 'admin'
-  private readonly validPassword = 'admin'
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  login(username: string, password: string): { message: string; token?: string } {
-    if (username === this.validUsername && password === this.validPassword) {
-      // In a real application, you would generate a JWT token here
-      return { message: 'Login successful', token: 'mocked-jwt-token' }
-    } else {
-      throw new UnauthorizedException('Invalid credentials')
+  async login(email: string, password: string): Promise<{ access_token: string }> {
+    // Find user by username in MongoDB
+    const user = await this.userModel.findOne({ email }).exec();
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
     }
+
+    // Compare the provided password with the stored password hash
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { id: user._id, username: user.username,  role: user.role };
+    const access_token = this.jwtService.sign(payload);
+
+    return { access_token };
   }
 }
