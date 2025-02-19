a88f8b2d09bc9d7719a6426784d298a138093847
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-19 12:26:45 +0700

    add swagger

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { CreateUserDto } from '../dtos/user.dto';
-
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'User Login' }) 
   async login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
   }
 
   @Post('register')
+  @ApiOperation({ summary: 'User Registration' }) 
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app.module'
 import * as dotenv from 'dotenv';
-import { ChildProcess } from 'child_process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   dotenv.config();
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for my NestJS project')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 4000)
 }
 bootstrap()
