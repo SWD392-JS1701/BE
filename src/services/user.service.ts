bfb995525d3a9188af51110f0cad384e183d0c8a
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-22 22:54:55 +0700

    set role

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -11,8 +11,10 @@ export class UserService {
   /* Create a new user */
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const role = createUserDto.role ?? 'user';
     const newUser = await this.userRepository.create({
       ...createUserDto,
+      role,
       passsword: hashedPassword,
     });
     return newUser;
