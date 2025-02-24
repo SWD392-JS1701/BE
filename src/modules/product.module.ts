da2d0577b749ce5ac7dc5bbb39e6a4e004868b33
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-02-24 16:20:13 +0700

    fix problem with module when change to repo

--- src/modules/product.module.ts
+++ src/modules/product.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { ProductsService } from '../services/product.service'
 import { ProductsController } from '../controllers/product.controller'
 import { Product, ProductSchema } from '../models/product.model'
+import { ProductRepository } from '~/repositories/product.repository'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService, ProductRepository],
+  exports: [ProductRepository]
 })
 export class ProductsModule {}
--- src/modules/promotedProduct.module.ts
+++ src/modules/promotedProduct.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { PromotedProductController } from '~/controllers/promotedProduct.controller'
 import { PromotedProduct, PromotedProductSchema } from '~/models/promotedProduct.model'
+import { PromotedProductRepository } from '~/repositories/promotedProduct.repository'
 import { PromotedProductService } from '~/services/promotedProduct.service'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: PromotedProduct.name, schema: PromotedProductSchema }])],
   controllers: [PromotedProductController],
-  providers: [PromotedProductService]
+  providers: [PromotedProductService, PromotedProductRepository],
+  exports: [PromotedProductRepository]
 })
 export class PromotedProductModule {}
