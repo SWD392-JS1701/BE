b42825a7469016004c0f242ca8f1d2b029a24c9e
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-03-17 19:44:34 +0700

    get doctor schedule

--- src/repositories/schedule.repository.ts
+++ src/repositories/schedule.repository.ts
@@ -1,59 +1,65 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Schedule, ScheduleDocument, Slot } from '../models/schedule.model';
-import { UpdateScheduleDto, UpdateSlotDto } from '~/dtos/schedule.dto';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { Schedule, ScheduleDocument, Slot } from '../models/schedule.model'
+import { UpdateScheduleDto, UpdateSlotDto } from '~/dtos/schedule.dto'
 
 @Injectable()
 export class ScheduleRepository {
-  constructor(
-    @InjectModel(Schedule.name) private scheduleModel: Model<ScheduleDocument>,
-  ) {}
+  constructor(@InjectModel(Schedule.name) private scheduleModel: Model<ScheduleDocument>) {}
 
   async create(schedule: Partial<Schedule>): Promise<ScheduleDocument> {
-    const newSchedule = new this.scheduleModel(schedule);
-    return newSchedule.save();
+    const newSchedule = new this.scheduleModel(schedule)
+    return newSchedule.save()
   }
 
   async findAll(): Promise<ScheduleDocument[]> {
-    return this.scheduleModel.find().exec();
+    return this.scheduleModel.find().exec()
   }
 
   async findById(id: string): Promise<ScheduleDocument | null> {
-    return this.scheduleModel.findById(id).exec();
+    return this.scheduleModel.findById(id).exec()
   }
 
   async findSlotById(dayOfWeek: string, slotId: string): Promise<Slot | null> {
-    const schedule = await this.scheduleModel
-      .findOne(
-        { dayOfWeek, 'slots.slotId': slotId },
-        { 'slots.$': 1 } 
-      )
-      .exec();
+    const schedule = await this.scheduleModel.findOne({ dayOfWeek, 'slots.slotId': slotId }, { 'slots.$': 1 }).exec()
 
-    return schedule && schedule.slots && schedule.slots.length > 0
-      ? schedule.slots[0]
-      : null;
+    return schedule && schedule.slots && schedule.slots.length > 0 ? schedule.slots[0] : null
   }
 
-  async update(id: string, updateData : UpdateScheduleDto): Promise<ScheduleDocument | null> {
-    return this.scheduleModel
-      .findByIdAndUpdate(id, updateData, { new: true })
-      .exec();
+  async update(id: string, updateData: UpdateScheduleDto): Promise<ScheduleDocument | null> {
+    return this.scheduleModel.findByIdAndUpdate(id, updateData, { new: true }).exec()
   }
 
   async updateSlot(scheduleId: string, slotId: string, updateData: UpdateSlotDto): Promise<Schedule> {
-    const schedule = await this.scheduleModel.findById(scheduleId);
+    const schedule = await this.scheduleModel.findById(scheduleId)
     if (!schedule) {
-      throw new NotFoundException(`Schedule with ID ${scheduleId} not found`);
+      throw new NotFoundException(`Schedule with ID ${scheduleId} not found`)
     }
 
-    const slot = schedule.slots.find(slot => slot.slotId === slotId);
+    const slot = schedule.slots.find((slot) => slot.slotId === slotId)
     if (!slot) {
-      throw new NotFoundException(`Slot with ID ${slotId} not found in schedule ${scheduleId}`);
+      throw new NotFoundException(`Slot with ID ${slotId} not found in schedule ${scheduleId}`)
     }
 
-    Object.assign(slot, updateData);
-    return await schedule.save();
+    Object.assign(slot, updateData)
+    return await schedule.save()
+  }
+
+  async findAllSlotsByDoctorId(doctorId: string): Promise<Slot[]> {
+    const schedules = await this.scheduleModel
+      .find({
+        'slots.doctorId': doctorId
+      })
+      .exec()
+
+    const matchingSlots: Slot[] = []
+
+    for (const schedule of schedules) {
+      const slots = schedule.slots.filter((slot) => slot.doctorId === doctorId)
+      matchingSlots.push(...slots)
+    }
+
+    return matchingSlots
   }
 }
