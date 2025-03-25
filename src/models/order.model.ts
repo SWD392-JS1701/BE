759a8ee2261e8db132f8ba8aaea87dc52ef7775f
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-03-25 09:35:08 +0700

    change style

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -4,25 +4,25 @@ import { HydratedDocument, Mongoose, Types } from 'mongoose'
 export type OrderDocument = HydratedDocument<Order>
 @Schema({ timestamps: true })
 export class Order {
-    @Prop({ required: true })
-    user_Id!: string
+  @Prop({ required: true })
+  user_Id!: string
 
-    @Prop({ required: true })
-    user_fullname!: string
+  @Prop({ required: true })
+  user_fullname!: string
 
-    @Prop({ required: true })
-    user_telephone!: string
+  @Prop({ required: true })
+  user_telephone!: string
 
-    @Prop({ required: true })
-    user_address!: string
+  @Prop({ required: true })
+  user_address!: string
 
-    @Prop()
-    amount!: number
+  @Prop()
+  amount!: number
 
-    @Prop({default: 1})
-    status!: number
+  @Prop({ default: 1 })
+  status!: number
 
-    createdAt!: Date
-    updatedAt!: Date
+  createdAt!: Date
+  updatedAt!: Date
 }
 export const OrderSchema = SchemaFactory.createForClass(Order)
