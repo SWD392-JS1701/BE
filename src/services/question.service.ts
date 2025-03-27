813aaec41a298523d457c94e127ab4fc51a8138e
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-03-27 22:29:42 +0700

    add check duplicate for promotion, promoted product

--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
-import { QuestionRepository } from '../repositories/question.repository';
-import { CreateQuestionDto, UpdateQuestionDto } from '../dtos/question.dto';
-import { Question } from '../models/question.model';
-import { Types } from 'mongoose';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common'
+import { QuestionRepository } from '../repositories/question.repository'
+import { CreateQuestionDto, UpdateQuestionDto } from '../dtos/question.dto'
+import { Question } from '../models/question.model'
+import { Types } from 'mongoose'
 
 @Injectable()
 export class QuestionService {
@@ -10,35 +10,34 @@ export class QuestionService {
 
   async createQuestion(createQuestionDto: CreateQuestionDto): Promise<Question> {
     if (!Types.ObjectId.isValid(createQuestionDto.quiz_ID)) {
-      throw new BadRequestException(`Invalid Quiz ID format: ${createQuestionDto.quiz_ID}`);
+      throw new BadRequestException(`Invalid Quiz ID format: ${createQuestionDto.quiz_ID}`)
     }
 
     const questionData = {
       ...createQuestionDto,
-      quiz_ID: new Types.ObjectId(createQuestionDto.quiz_ID),
-    };
-  
-    return this.questionRepository.create(questionData);
+      quiz_ID: new Types.ObjectId(createQuestionDto.quiz_ID)
+    }
+
+    return this.questionRepository.create(questionData)
   }
-  
 
   async getAllQuestions(): Promise<Question[]> {
-    return this.questionRepository.findAll();
+    return this.questionRepository.findAll()
   }
 
   async getQuestionById(id: string): Promise<Question> {
-    const question = await this.questionRepository.findById(id);
-    if (!question) throw new NotFoundException(`Question with ID ${id} not found`);
-    return question;
+    const question = await this.questionRepository.findById(id)
+    if (!question) throw new NotFoundException(`Question with ID ${id} not found`)
+    return question
   }
 
   async updateQuestion(id: string, updateQuestionDto: UpdateQuestionDto): Promise<Question> {
-    const updatedQuestion = await this.questionRepository.update(id, updateQuestionDto);
-    if (!updatedQuestion) throw new NotFoundException(`Question with ID ${id} not found`);
-    return updatedQuestion;
+    const updatedQuestion = await this.questionRepository.update(id, updateQuestionDto)
+    if (!updatedQuestion) throw new NotFoundException(`Question with ID ${id} not found`)
+    return updatedQuestion
   }
 
   async deleteQuestion(id: string): Promise<void> {
-    return this.questionRepository.delete(id);
+    return this.questionRepository.delete(id)
   }
 }
